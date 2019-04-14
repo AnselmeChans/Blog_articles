eb0220b6471485489c7c9792220772e5e786a0d3
hasouuu cricri <anselmeceril@gmail.com>
2019-04-14 18:23:14 +0200

    add models and admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from django.forms import Textarea
+from django.db import models
+from blog.models import Post, PostCategory, Comment
 
 # Register your models here.
+@admin.register(PostCategory)
+class PostCategoryAdmin(admin.ModelAdmin) : 
+    search_fields = ['name']
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin) : 
+        list_display = (
+                        'title', 
+                        'category', 
+                        'published', 
+                        'text',
+                        'created_at',
+                        'comments_count',
+                        )
+
+        list_filter = (
+            'category__name', 
+            'published',
+        )
+
+        autocomplete_fields = ['category']
+
+        formfield_overrides = {
+            models.TextField: {'widget': Textarea(attrs={'rows': 20, 'cols': 90})}
+        }
+
+        def comments_count(self, obj) : 
+            # Pour chaque post cb de commentaires ont été crée
+            return Comment.objects.filter(post=obj).count()
+
+        comments_count.short_description='Comments'
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+class PostCategory(models.Model): 
+    name = models.CharField(max_length=50)
+
+    def __str__(self) : 
+        return self.name 
+
+class Post(models.Model) : 
+    title = models.CharField(max_length=100)
+    category = models.ForeignKey('PostCategory', 
+                                null=True, 
+                                blank=True, 
+                                on_delete= models.DO_NOTHING)
+    published = models.BooleanField(default=False)
+    text = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) : 
+        return self.title
+
+class Comment(models.Model) : 
+    STATUS_VISIBLE  = 'visible'
+    STATUS_HIDDEN = 'hidden'
+    STATUS_MODERTED = 'moderated'
+
+    STATUS_CHOICE = (
+        (STATUS_VISIBLE, 'Visible'),
+        (STATUS_HIDDEN, 'Hidden'),
+        (STATUS_MODERTED, 'Moderated'),
+    )
+    
+    post = models.ForeignKey("Post", 
+                            on_delete=models.CASCADE
+                            )
+    author = models.CharField(max_length=50)
+    text = models.TextField()
+    status= models.CharField(max_length=50, 
+                            default=STATUS_VISIBLE, 
+                            choices=STATUS_CHOICE)
+
+    moderation_text = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.author
+    
